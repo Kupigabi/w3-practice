61f5376c3fd27278c90c69a383443a74b7ed356b
Kupigabi <gabor.kupeczki@gmail.com>
2023-10-13 09:31:13 +0200

    changed-script.js

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-let arrOfNumbers = [10, 20, 30, 40, 50, 60]
+const exampleArr = [5, -2, 4, -10, -1, 14]
+const positiveSum = (cats) => cats.filter((cat) => cat > 0).length
+console.log(positiveSum(exampleArr))
 
-/* for (let i = 0; i < arr.length; i++) {
-  let result = arr[i] * 2
-  console.log(result)
-} */
+const buggyCatNames = ["xFelixy", "xHuntery", "xSanyiy", "xPistay", "xCirmosy", "xCsipeszy", "xWhiskersy", "xFütyiy", "xSlukky", "xBélay", "xBundásy", "xKormiy", "xCirmiy"]
+const removeChar = (names) => names.map(name => name.substring(1, name.length - 1))
+console.log(removeChar(buggyCatNames))
 
-let newArr = []
-arrOfNumbers.forEach((number) => newArr.push(number * 2))
-console.log(newArr)
+const exampleCatAges = [27, 4, 8, 11, 2, 9, 39]
+const youngestCat = (catAges) => catAges.sort((a, b) => a - b)[0]
+console.log(youngestCat(exampleCatAges))
+
+const exampleCatId = 123456
+const createId = (catId) => String(catId).split("").reverse().map(numChar => Number(numChar))
+console.log(createId(exampleCatId))
+
+const foodListExample = [1,3,5,6,7,8]
+const foodTime = (foodList) => Array.from({length: foodList[foodList.length - 1] - foodList[0] + 1})
+    .map((element, index) => index + foodList[0])
+console.log(foodTime(foodListExample))
+
